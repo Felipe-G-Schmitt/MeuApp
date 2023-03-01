651751470ee6e99b98f61c0907b0a0cc84b544d8
Felipe-G-Schmitt <felipegabrielsc2006@gmail.com>
2023-03-01 16:45:07 -0300

    refiz o codigo e navegação

--- App.js
+++ App.js
@@ -1,44 +1,5 @@
-import * as React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { StyleSheet} from "react-native";
-import { A } from "@expo/html-elements";
-import HomeScreen from "./src/screens/HomeScreen";
-import DetailsScreen from "./src/screens/DetailsScreen";
-
-const Stack = createNativeStackNavigator();
+import RootNavigation from "./src";
 
 export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{ title: "Página Home" }}
-        />
-        <Stack.Screen
-          name="Details"
-          component={DetailsScreen}
-          options={{ title: "Página details" }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+  return <RootNavigation />;
 }
-
-/**
- * <div style="flex:1; background-color: #FFF; align-items: center; justify-content:center">
- *  <p>Opne Up...</p>
- * </div>
- * não existe " - " no js, é tudo junto e letra maiuscula
- */
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
