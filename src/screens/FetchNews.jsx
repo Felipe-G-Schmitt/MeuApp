b2905056d0dc5f108aa4679a36de5c45af533ebb
Felipe-G-Schmitt <felipegabrielsc2006@gmail.com>
2023-03-28 14:54:33 -0300

    api fetch

--- src/screens/FetchNews.jsx
+++ src/screens/FetchNews.jsx
@@ -1,26 +1,31 @@
+import { useState } from "react";
 import { useEffect } from "react";
-import { View } from "react-native";
+import { ScrollView, View } from "react-native";
 import { Text } from "react-native-paper";
 import styles from "../utils/styles";
 
-
 export default function FetchNews() {
+  const [data, setData] = useState([]);
 
-    function fetchNewsData(){
-        fetch('https://jsonplaceholder.typicode.com/posts')
-        .then((response) => response.json())
-        .then((json) => console.log(json))
-        .catch((error) => console.error(error));
-    }
+  function fetchNewsData() {
+    fetch("https://rickandmortyapi.com/api/character")
+      .then((response) => response.json()) // transforma a informação em JSON
+      .then((json) => setData(json)) // mostra o resultado no console em JSON
+      .catch((error) => console.error(error)); // mostra o erro no console
+  }
 
-    useEffect(() => {
-        fetchNewsData();
-    }, []);
+  useEffect(() => {
+    fetchNewsData();
+  }, []);
 
-    return(
-        <View style={styles.container}>
-            <Text>Fetch News</Text>
-        </View>
-    )
-
+  return (
+    <ScrollView>
+    <View style={styles.container}>
+        <Text style={{fontSize: '30px', fontWeight:'bold', margin: '15px'}}>Fetch News</Text>
+            {data.results?.map((item) => (
+                <Text style={{ fontSize: '25px', margin: '5px'}} key={item.id}>{item.name} | {item.status} | {item.species} | {item.gender}</Text>
+            ))}
+    </View>
+    </ScrollView>
+  );
 }
