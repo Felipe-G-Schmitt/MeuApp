61f903375f754aff14ac029b2cd5ad7387fb3a87
Felipe-G-Schmitt <felipegabrielsc2006@gmail.com>
2023-04-19 16:19:56 -0300

    Whatsapp

--- src/screens/RMGameScreen.jsx
+++ src/screens/RMGameScreen.jsx
@@ -4,90 +4,128 @@ import { Button, Text } from "react-native-paper";
 import { styles } from "../utils/styles";
 
 export default function RMGameScreen() {
-    const [personagem, setPersonagem] = useState(null);
-    const [personagens, setPersonagens] = useState([]);
-    const [totalPersonagens, setTotalPersonagens] = useState(1);
-    var Resultado;
+  const [personagem, setPersonagem] = useState(null);
+  const [totalPersonagens, setTotalPersonagens] = useState(1);
+  const [resultado, setResultado] = useState(null);
+  const [pontuacao, setPontuacao] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
 
-    useEffect(() => {
-        fetch('https://rickandmortyapi.com/api/character')
-            .then((response) => response.json())
-            .then((json) => {
-                setTotalPersonagens(json.info.count);
-            })
-    }, []);
+  useEffect(() => {
+    fetch("https://rickandmortyapi.com/api/character")
+      .then((response) => response.json())
+      .then((json) => {
+        setTotalPersonagens(json.info.count);
+      });
+  }, []);
 
-    useEffect(() => {
-        fetch('https://rickandmortyapi.com/api/character/' + returnRandomNumber())
-            .then((response) => response.json())
-            .then((json) => {
-                setPersonagem(json);
-            })
-    }, [totalPersonagens]);
+  useEffect(() => {
+    BuscarPerson();
+  }, [totalPersonagens]);
 
-    function BuscarPerson() {
-        fetch('https://rickandmortyapi.com/api/character/' + returnRandomNumber())
-            .then((response) => response.json())
-            .then((json) => {
-                setPersonagem(json);
-            })
+  function BuscarPerson() {
+    fetch("https://rickandmortyapi.com/api/character/" + returnRandomNumber())
+      .then((response) => response.json())
+      .then((json) => {
+        setPersonagem(json);
+        setResultado(null);
+      });
+  }
+
+  async function handlePersonagemVivoOuMorto(resposta) {
+    setIsLoading(true);
+    const isAlive = personagem.status === "Alive";
+    if (resposta === isAlive) {
+      setResultado("Parabéns, você acertou!");
+      setPontuacao(pontuacao + 1);
+    } else {
+      setResultado("Que pena, você errou!");
+      setPontuacao(pontuacao - 1);
     }
+    setTimeout(() => {
+      BuscarPerson();
+      setIsLoading(false);
+    }, 500);
+  }
 
-    async function handlePersonagemVivoOuMorto(resposta) {
-        const isAlive = personagem.status === 'Alive';
-        if (resposta === isAlive) {
-            var Resultado='O personagem está morto!';
-        } else {
-            var Resultado='O personagem está vivo!';
-        }
-        console.log(Resultado);
-        
-        BuscarPerson();
+  const returnRandomNumber = () => {
+    let randomNumber = Math.floor(Math.random() * totalPersonagens) + 1;
+
+    // canoot return 0
+    if (randomNumber === 0) {
+      return 1;
     }
+    return randomNumber;
+  };
 
-    function Resultado(Resultado){
-        return Resultado;
-    }
-
-    const returnRandomNumber = () => {
-        let randomNumber = Math.floor(Math.random() * totalPersonagens) + 1;
-
-        // canoot return 0
-        if (randomNumber === 0) {
-            return 1;
-        }
-        return randomNumber;
-    }
-
-    return (
-        <View style={styles.container}>
-        <View style={{ border: "3px solid black", alignItems: "Center", padding: 30, borderRadius: 10}}>
-            <Text style={styles.title}>Rick and Morty Game</Text>
-            <Text style={styles.subtitle}>Você sabe este personagem está vivo?</Text>
-            {personagem && (
-                <View>
-                    <View style={{alignItems: "center", justifyContent: "center", textAlign: "center",}}>
-                        <Image source={{ uri: personagem.image }} style={{ width: 200, height: 200, marginTop: 20,}} />
-                    </View>
-                    <Text style={{ fontSize: 25, textAlign: "center", marginVertical: 20 }}>
-                        Como o/a {personagem.name} está?
-                    </Text>
-                    <View style={{ alignItems: "center", flexDirection: "row", justifyContent: "center", textAlign: "center"}}>
-                        <View style={{ alignItems: "center", marginRight: 20}}>
-                        <Button
-                            mode="contained"
-                            onPress={() => handlePersonagemVivoOuMorto(true)}
-                        >Vivo</Button>
-                        </View>
-                        <Button
-                            mode="contained"
-                            onPress={() => handlePersonagemVivoOuMorto(false)}
-                        >Morto</Button>
-                        <Text>{Resultado}</Text>
-                    </View>
-                </View>
+  return (
+    <View style={styles.container}>
+      <View
+        style={{
+          border: "7px double black",
+          alignItems: "Center",
+          padding: 30,
+          borderRadius: 10,
+        }}
+      >
+        <Text style={styles.title}>Rick and Morty Game</Text>
+        <Text style={styles.subtitle}>
+          Você sabe este personagem está vivo?
+        </Text>
+        {personagem && (
+          <View>
+            <View
+              style={{
+                alignItems: "center",
+                justifyContent: "center",
+                textAlign: "center",
+              }}
+            >
+              <Image
+                source={{ uri: personagem.image }}
+                style={{ width: 200, height: 200, marginTop: 20, border: "5px black solid", borderRadius: 20 }}
+              />
+            </View>
+            <Text
+              style={{ fontSize: 25, textAlign: "center", marginVertical: 20 }}
+            >
+              Como o/a {personagem.name} está?
+            </Text>
+            <View
+              style={{
+                alignItems: "center",
+                flexDirection: "row",
+                justifyContent: "center",
+                textAlign: "center",
+              }}
+            >
+              <View style={{ alignItems: "center", marginRight: 20 }}>
+                <Button
+                  mode="contained"
+                  onPress={() => handlePersonagemVivoOuMorto(true)}
+                  disabled={isLoading}
+                >
+                  Vivo
+                </Button>
+              </View>
+              <View style={{ alignItems: "center", marginRight: 20 }}>
+                <Button
+                  mode="contained"
+                  onPress={() => handlePersonagemVivoOuMorto(false)}
+                  disabled={isLoading}
+                >
+                  Morto
+                </Button>
+              </View>
+            </View>
+            {resultado && (
+              <Text style={{ textAlign: "center", marginVertical: 20 }}>
+                {resultado}
+              </Text>
             )}
-        </View>
-        </View>
-    );
+            <Text style={{ textAlign: "center", marginVertical: 20 }}>Pontuação: {pontuacao}</Text>
+          </View>
+        )}
+      </View>
+    </View>
+  );
 }
